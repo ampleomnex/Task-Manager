7959d5c280e284223f410a65702fdcf4f3e911f3
Pradeesh NarayananKutty <pnarayananKutty@omnex.com>
2022-07-07 18:36:52 +0530

    Project creation bug fix

---
+++ TaskManager/Models/Request/ProjectRequest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager.Models.Request
+{
+    public class ProjectRequest
+    {
+        public string ProjectName { get; set; }
+
+        public String spoc { get; set; }
+
+        public int CustomerID { get; set; }
+
+    }
+}
