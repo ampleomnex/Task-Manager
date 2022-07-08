41a37eea4cd5778d6719540ff3abe0b51731558c
Remya T <rthiyyath@omnex.com>
2022-07-08 19:07:08 +0530

    List All Tasks Report without any condition.

---
+++ TaskManager/Controllers/TaskReportController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System.Collections;
+using System.Data;
+using TaskManager.Data;
+using TaskManager.Models;
+using TaskManager.Models.Response;
+
+namespace TaskManager.Controllers
+{
+    public class TaskReportController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<AppUser> _userManager;
+        public TaskReportController(ApplicationDbContext context,UserManager<AppUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            return View(getTaskReport().ToList());
+        }
+
+             
+        [HttpGet]
+        public List<EmployeeTaskReport> getTaskReport ()
+        {
+            List<EmployeeTaskReport> employees = new List<EmployeeTaskReport>();
+            try
+            {
+                
+                using (var database = _context)
+                {
+                    var connection = _context.Database.GetDbConnection();
+                    connection.Open();
+
+                    var command = connection.CreateCommand();
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.CommandText = "[dbo].[TaskReport]";
+                    var reader = command.ExecuteReader();
+
+                    
+                    while (reader.Read())
+                    {
+                        EmployeeTaskReport employeeTaskReport = new EmployeeTaskReport();
+                        employeeTaskReport.ProjectName = reader.IsDBNull("ProjectName") ? null : reader.GetString("ProjectName");
+                        employeeTaskReport.EpicName = reader.IsDBNull("EpicsName") ? null : reader.GetString("EpicsName");
+                        employeeTaskReport.Priority = reader.IsDBNull("Priority") ? null : reader.GetString("Priority");
+                        employeeTaskReport.AssignedTo = reader.IsDBNull("AssignedTo") ? null : reader.GetString("AssignedTo");
+                        employeeTaskReport.RequestedBy = reader.IsDBNull("RequestedBy") ? null : reader.GetString("RequestedBy");
+                        employeeTaskReport.TaskName = reader.IsDBNull("TaskName") ? null : reader.GetString("TaskName");
+                        employeeTaskReport.Status = reader.IsDBNull("Status") ? null : reader.GetString("Status");
+                        employeeTaskReport.PlannedStart = reader.GetDateTime("PlannedStart");
+                        employeeTaskReport.RequestDate = reader.GetDateTime("RequestDate");
+                        employeeTaskReport.ModifiedDate =  reader.GetDateTime("ModifiedDate");
+                        employeeTaskReport.DueDate = reader.GetDateTime("DueDate");
+                        employees.Add(employeeTaskReport);  
+                    }
+
+                    reader.Close();
+                }
+                return employees;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }        
+    }
+}
---
+++ TaskManager/Models/Response/EmployeeTaskReport.cs
@@ -0,0 +1,26 @@
+﻿namespace TaskManager.Models.Response
+{
+    public class EmployeeTaskReport
+    {
+        public string ProjectName { get; set; }   
+        public string EpicName { get; set; }    
+        public string Priority { get; set; }
+
+        public string AssignedTo { get; set; }    
+
+        public string RequestedBy { get; set; }
+
+        public string TaskName { get; set; }
+
+        public string Status { get; set; }
+
+        public DateTime PlannedStart { get; set; }
+
+        public DateTime RequestDate { get; set; }
+
+        public DateTime ModifiedDate { get; set; }
+
+        public DateTime DueDate { get; set; }
+
+    }
+}
