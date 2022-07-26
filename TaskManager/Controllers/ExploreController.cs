7ed9885465bcb280e4fae775738dc3e6d9364599
Remya T <rthiyyath@omnex.com>
2022-07-26 14:39:48 +0530

    Showing AMPLE courses in explore screen.

---
+++ TaskManager/Controllers/ExploreController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RestSharp;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text;
+using TaskManager.Models.Response;
+
+namespace TaskManager.Controllers
+{
+    public class ExploreController : Controller
+    {
+        public ExploreController()
+        {
+
+        }
+
+        public class ExploreCourseParams
+        {
+            public int numOfRecordsPerPage { get; set; }
+            public int pageNo { get; set; }
+            public string? batchStatus { get; set; }
+        }
+
+
+        // [HttpPost]
+        public async Task<IActionResult> IndexAsync()
+        {
+            var data= "numOfRecordsPerPage=100&pageNo=0&batchStatus=" + "40,10";
+
+            var exploreParams = new ExploreCourseParams
+            {
+                numOfRecordsPerPage = 100,
+                pageNo = 0,
+                batchStatus = "40,10"
+            };
+
+            var json = JsonConvert.SerializeObject(exploreParams);
+            //var exdata = new StringContent(json, Encoding.UTF8, "application/json");
+            var exdata = new StringContent(data, Encoding.UTF8, "application/x-www-form-urlencoded");
+
+            List<Courses> CoursesInfo = new List<Courses>();
+           
+            var url = "https://ample.omnex.com/explore";
+            using var client = new HttpClient();
+
+            //var response = await client.PostAsync(url, paramdata);
+            var response = await client.PostAsync(url, exdata);
+
+            //Storing the response details recieved from web api
+            var ExploreResponse = response.Content.ReadAsStringAsync().Result;
+            //Deserializing the response recieved from web api and storing into the Course list
+            CoursesInfo = JsonConvert.DeserializeObject<List<Courses>>(ExploreResponse); 
+            foreach(var items in CoursesInfo)
+            {
+                items.BatchStartDate = (new DateTime(1970, 1, 1)).AddMilliseconds(double.Parse(items.BatchStartDate)).ToString("MMM dd, yyyy");
+                items.BatchEndDate = (new DateTime(1970, 1, 1)).AddMilliseconds(double.Parse(items.BatchEndDate)).ToString("MMM dd, yyyy");
+                if (items.CourseImage != null)
+                {
+                    items.CourseImageFullPath = "https://ample.omnex.com/resources/userspace/Courses/" + items.CourseID + "/Images/Framework/" + items.CourseImage;
+                }
+                else
+                {
+                    items.CourseImageFullPath = "https://ample.omnex.com/resources/product/images/NewIcons/svg/course.svg";
+                }
+            }
+            return View(CoursesInfo);
+
+        }
+    }
+}
---
+++ TaskManager/Models/Response/Courses.cs
@@ -0,0 +1,36 @@
+﻿namespace TaskManager.Models.Response
+{
+    public class Courses
+    {
+        public string CourseName { get; set; }
+        public int Amount { get; set; } 
+
+        public string BatchCourseID { get; set; }
+
+        public string BatchEndDate { get; set; }
+
+        public string BatchID { get; set; }
+
+        public string BatchName { get; set; }
+
+        public string BatchStartDate { get; set; }
+
+        public string CategoryID { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public string CentreName { get; set; }
+
+        public string Clid { get; set; }
+
+        public string CourseImage { get; set; }
+
+        public string CourseID { get; set; }
+
+        public string InstructorName { get; set; }
+
+        public string CourseImageFullPath { get; set; }
+
+
+    }
+}
